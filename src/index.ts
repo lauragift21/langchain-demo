eaa7eabbca11d157161d555ceef28436f2a01cd9
lauragift21 <laurandidi21@gmail.com>
2023-09-20 14:21:07 +0200

    initial commit

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,28 @@
-import { OpenAI } from 'langchain/llms/openai';
+import { S3Loader } from 'langchain/document_loaders/web/s3';
 
 export interface Env {
+	ACCESS_KEY: string;
+	ACCESS_SECRET_KEY: string;
+	UNSTRUCTURED_API_KEY: string;
 }
 
 export default {
 	async fetch(request: Request, env: Env, ctx: ExecutionContext): Promise<Response> {
-		return new Response('Hello World!');
+		const loader = new S3Loader({
+			bucket: 'lauragift-bucket',
+			key: 'gift.png',
+			s3Config: {
+				region: 'us-east-1',
+				credentials: {
+					accessKeyId: env.ACCESS_KEY,
+					secretAccessKey: env.ACCESS_SECRET_KEY,
+				},
+			},
+			unstructuredAPIKey: env.UNSTRUCTURED_API_KEY,
+			unstructuredAPIURL: 'https://api.unstructured.io/general/v0/general',
+		});
+		const docs = await loader.load();
+		console.log(docs);
+		return new Response('Hello World!', { status: 200});
 	},
 };
