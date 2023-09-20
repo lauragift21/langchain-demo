75e13c278f9e532d9120d2450a3bca43129f8d9a
lauragift21 <laurandidi21@gmail.com>
2023-09-20 10:07:16 +0200

    initial commit

---
+++ src/index.ts
@@ -0,0 +1,10 @@
+import { OpenAI } from 'langchain/llms/openai';
+
+export interface Env {
+}
+
+export default {
+	async fetch(request: Request, env: Env, ctx: ExecutionContext): Promise<Response> {
+		return new Response('Hello World!');
+	},
+};
